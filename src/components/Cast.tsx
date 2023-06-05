ad1b4673fb58ab51ecab5656a3ba5366038f169d
santoshchhn6 <santoshchhn6@gmail.com>
2023-06-05 18:46:55 +0530

    Added style to Cast Component

--- src/components/Cast.tsx
+++ src/components/Cast.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, appDispatch } from "../redux";
 import { fetchCast } from "../redux/movie_action";
+import useWindowSize from "../customHooks/useWindowSize";
+import Arrow from "./Buttons/Arrow";
 
 type Props = {
   movieId: number;
@@ -14,20 +16,80 @@ const Cast = ({ movieId }: Props) => {
     dispatch(fetchCast(movieId));
   }, [movieId, dispatch]);
 
+  const { width } = useWindowSize();
+  const [disableLeftArrow, setDisableLeftArrow] = useState<boolean>(true);
+  const [disableRightArrow, setDisableRightArrow] = useState<boolean>(false);
+  const [translateX, setTranslateX] = useState<number>(0);
+  const poster_width = 200;
+  const poster_height = 300;
+  const poster_gap = 20;
+  const totalPostersLenth = actors.length * (poster_width + poster_gap);
+
+  const handleLeftClick = () => {
+    if (translateX - width >= 0) {
+      setTranslateX((translateX) => translateX - width);
+      setDisableRightArrow(false);
+    }
+    if (translateX - 2 * width < 0) {
+      setTranslateX(0);
+      setDisableLeftArrow(true);
+    }
+  };
+
+  const handleRightClick = () => {
+    if (translateX + width <= totalPostersLenth) {
+      setTranslateX((translateX) => translateX + width);
+      setDisableLeftArrow(false);
+    }
+  };
+
   return (
-    <div>
-      <h2>Cast</h2>
-      <div>
-        {actors.map((actor) => (
-          <div key={actor.id}>
-            <img
-              src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
-              alt={actor.name}
-            />
-            <p>{actor.name}</p>
-            <p>{actor.character}</p>
-          </div>
-        ))}
+    <div className="p-5">
+      <h2 className="mb-5 text-[24px] font-['Poppin-sb'] text-blue-500">
+        Cast
+      </h2>
+      <div className=" w-[100%] h-[400px] relative overflow-hidden">
+        <div
+          className=" absolute flex gap-5 text-center ease-in-out duration-500"
+          style={{
+            translate: `-${translateX}px 0px`,
+            gap: `${poster_gap}px`,
+          }}
+        >
+          {actors.map((actor) => (
+            <div key={actor.id} className="">
+              <img
+                src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
+                alt={actor.name}
+                className="min-w-[200px] min-h-[300px] rounded-lg"
+              />
+              <p className="text-[18px] font-['Poppin-sb'] my-2">
+                {actor.name}
+              </p>
+              <p>{actor.character}</p>
+            </div>
+          ))}
+        </div>
+        <div
+          className=" absolute left-3"
+          style={{ top: `${poster_height / 2}px` }}
+        >
+          <Arrow
+            direction="left"
+            onClick={handleLeftClick}
+            disabled={disableLeftArrow}
+          />
+        </div>
+        <div
+          className={` absolute right-3`}
+          style={{ top: `${poster_height / 2}px` }}
+        >
+          <Arrow
+            direction="right"
+            onClick={handleRightClick}
+            disabled={disableRightArrow}
+          />
+        </div>
       </div>
     </div>
   );
